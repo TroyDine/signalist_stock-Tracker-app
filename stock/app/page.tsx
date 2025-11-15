79e18e0e55bd2f19998c60fffeccc1ffa8589c00
Dinesh K <your-kdinesh0612@gmail.com>
2025-11-15 14:18:04 +0530

    Initial commit

---
+++ stock/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className={"flex justify-center items-center h-screen"}>
+            <button>Click Me</button>
+        </div>
+    )
+}
+export default Home
