e85ed811751a745c566549fddb0fcf670a2494c8
Dinesh K <your-kdinesh0612@gmail.com>
2026-01-04 23:11:20 +0530

    Updated file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import NavItems from "@/components/NavItems";
+import NavItems from "@/components/Navitems";
 import UserDropdown from "@/components/UserDropdown";
 import {searchStocks} from "@/lib/actions/finnhub.action";
 
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -17,7 +17,7 @@ import {
     AvatarImage,
 } from "@/components/ui/avatar"
 import { LogOut } from "lucide-react"
-import NavItems from "@/components/NavItems"
+import NavItems from "@/components/Navitems"
 
 const UserDropdown = ({ user }: {user: User}) => {
     const router = useRouter();
