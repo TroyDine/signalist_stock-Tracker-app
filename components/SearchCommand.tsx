ece1fae812520e4c4a3c9eda3e15e6c70808eec6
Dinesh K <your-kdinesh0612@gmail.com>
2026-01-11 20:01:15 +0530

    commit

--- components/SearchCommand.tsx
+++ components/SearchCommand.tsx
@@ -99,7 +99,6 @@ export default function SearchCommand({ renderAs = 'button', label = 'Add stock'
                           {stock.symbol} | {stock.exchange } | {stock.type}
                         </div>
                       </div>
-                    {/*<Star />*/}
                     </Link>
                   </li>
               ))}
